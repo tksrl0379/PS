05c64458368cfe75929bf3210dfd0656cf07a469
Young Min Sim <tksrl0379@naver.com>
2021-02-06 21:38:06 +0900

    feat: 11497.swift

---
+++ BOJ/11497.swift
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/11497
+
+let n = Int(readLine()!)!
+
+let f = n/5
+var answer = -1
+
+
+for i in (0...f).reversed() {
+    
+    let left = n - (5 * i) // 5 배수 제하고 남은 수
+    
+    if Double(left / 2) == Double(left) / 2.0 {
+        answer = i + left / 2
+        
+        break
+    }
+}
+
+print(answer)
