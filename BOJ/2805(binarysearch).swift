23dd9f8f1822e62550a83cee0be646ec0410f5cb
YoungMin Sim <tksrl0379@naver.com>
2021-04-04 14:09:23 +0900

    Update 2805(binarysearch).swift

--- BOJ/2805(binarysearch).swift
+++ BOJ/2805(binarysearch).swift
@@ -1,35 +1,36 @@
 // https://www.acmicpc.net/problem/2805
-// 입력 받을 때 map 말고 compactMap 쓰자.
-
-import Foundation
-
-func totalHeight(cutHeight: Int) -> Int {
-    return trees.reduce(0) { $0 + (($1 > cutHeight) ? $1 - cutHeight : 0) }
-}
-
-func binarySearch(target: Int) -> Int {
-    var start = 1
-    var end = trees.max()!
-    var res = 0
-    
-    while end >= start {
-        let mid = (end + start) / 2
-        let totHeight = totalHeight(cutHeight: mid) 
-        
-        if target <= totHeight { // 이미 요구조건을 충족했어도 자르는 길이를 올려보는 것 (최대값을 구하는 문제이므로 upperbound)
-            res = mid
-            start = mid + 1
-        } else {
-            end = mid - 1
-        }
-    }
-    return res
-}
-
+// 1. 입력 받을 때 map 말고 compactMap 쓰자.
+// 2. 이 문제의 경우 Int(String($0)) 으로 변환해야 시간초과가 안 뜸. 좀 이상한듯.
 
 let input = readLine()!.split(separator: " ").compactMap { Int($0) }
 let M = input[1]
 
 let trees = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
 
-print(binarySearch(target: M))
+
+func cut(to height: Int) -> Int {
+    trees.reduce(0) { $0 + max(0, ($1 - height)) }
+}
+
+
+func binarySearch() -> Int {
+    var left = 0
+    var right = trees.max()!
+    var ans = 0
+    
+    while left <= right {
+        let mid = (left + right)/2
+        
+        if cut(to: mid) >= M {
+            left = mid + 1
+            ans = mid
+        } else {
+            right = mid - 1
+        }
+    }
+    
+    return ans
+}
+
+
+print(binarySearch())
