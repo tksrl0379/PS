b47551140ce67ab1be2c07db362cffb75bf859e2
YoungMin Sim <tksrl0379@naver.com>
2021-05-12 00:14:52 +0900

    Update Replace Elements with Greatest Element on Right Side.swift

--- leetcode/Replace Elements with Greatest Element on Right Side.swift
+++ leetcode/Replace Elements with Greatest Element on Right Side.swift
@@ -1,5 +1,20 @@
 // https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3259/
 
+// 일부러 비효율적으로 풀어보기 -> 시간초과
+
+class Solution {
+    func replaceElements(_ arr: [Int]) -> [Int] {
+        var arr = arr
+        for i in 0..<arr.count-1 {
+            arr[i] = arr[(i+1)...].max()!
+        }
+        arr[arr.count-1] = -1
+        return arr
+    }
+}
+
+// 정답
+
 class Solution {
     
     func replaceElements(_ arr: [Int]) -> [Int] {
