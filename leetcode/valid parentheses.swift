2cc2016bbe3dce1dad5a1a680426b53f9c29a79e
YoungMin Sim <tksrl0379@naver.com>
2021-11-09 21:32:10 +0900

    Create valid parentheses.swift

---
+++ leetcode/valid parentheses.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        if s.count == 1 { return false }
+        
+        let dict = ["}": "{", ")": "(", "]": "["]
+        var leftBracketStack = [String]()
+        
+        for chr in s {
+            let chr = String(chr)
+            
+            if let leftBracket = dict[chr] {
+                if leftBracket != leftBracketStack.popLast() {
+                    return false
+                }
+            } else {
+                leftBracketStack.append(chr)
+            }
+        }
+        
+        return leftBracketStack.isEmpty
+    }
+}
