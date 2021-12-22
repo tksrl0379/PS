2d53bb7e95b789af9c5b99d5be63396383f03c97
YoungMin Sim <tksrl0379@naver.com>
2021-12-22 23:17:38 +0900

    Update Remove Duplicates from Sorted Array.swift

--- leetcode/Remove Duplicates from Sorted Array.swift
+++ leetcode/Remove Duplicates from Sorted Array.swift
@@ -3,31 +3,34 @@
 // 1차 정답
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-        guard var recent = nums.first else {
-            return 0
+        var set = Set<Int>()
+        var index = 0
+        for i in (0..<nums.count) where !set.contains(nums[i]) {
+            set.insert(nums[i])
+            nums[index] = nums[i]
+            index += 1
         }
-        var cnt = 1
-        
-        for i in 1..<nums.count where nums[i] != recent {
-            nums[cnt] = nums[i]
-            recent = nums[i]
-            cnt += 1
-        }
-        return cnt
+
+        return index
     }
 }
 
+// 2차 정답
+// 현재의 index 앞에 있는 값들은 이미 검증이 끝났으므로 굳이 이전 값들을 모두 포함하는 Set을 사용할 필요가 없음. 가장 최근 index만 비교하면 됨
+// 문제 풀다 보면 이런거 놓치는 경우가 많은데 항상 염두에 둬야 할 듯.
 
-// 2차 정답: 필요 없는 변수 제거
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-        if nums.count <= 1 { return nums.count }
-        var cnt = 0
-        
-        for i in 1..<nums.count where nums[i] != nums[cnt] {
-            cnt += 1
-            nums[cnt] = nums[i]
+        guard !nums.isEmpty else {
+            return 0
         }
-        return cnt + 1
+        
+        var index = 0
+        for i in (1..<nums.count) where nums[index] != nums[i] {
+            index += 1
+            nums[index] = nums[i]
+        }
+        
+        return index + 1
     }
 }
